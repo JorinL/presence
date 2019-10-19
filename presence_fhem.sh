41298f65a2c19aa7baea6a31889e901f5547b3bc
JorinL <33031729+JorinL@users.noreply.github.com>
2019-10-20 01:17:21 +0200

    Add files via upload

---
+++ presence_fhem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# detect Iphone/Android by IP/HOSTNAME and MAC address.
+# returns 1 or 0 from mqtt broker to fhem
+
+
+# exit immediately if no parameters supplied
+if [ $# -lt 1 ]
+  then
+    echo "UNDEF"
+  exit 1
+fi
+
+# Set variables
+NAME=$1
+
+#MQTT without auth
+RESULT=`/usr/bin/mosquitto_sub -h MOSQUITTOSERVER -t "your/specific/topic/${NAME}" -C 1`
+#MQTT With auth:
+#RESULT=`/usr/bin/mosquitto_sub -h MOSQUITTOSERVER -u USER -P "PASSWORD" -t "your/specific/topic/${NAME}" -C 1`
+#RESULT='1'
+#echo "${RESULT}"
+if [ "${RESULT}" == "1" ]; then
+        echo "1"
+        exit 0
+else
+        echo "0"
+        exit 0
+fi
+done
